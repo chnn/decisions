514d36c460c3354e6007e78ac1e661032c50e772
Christopher Henn <chris@chrishenn.net>
2017-05-04 19:54:10 -0700

    Most of the frontend

--- app/components/video-layout/component.js
+++ app/components/video-layout/component.js
@@ -1,6 +1,45 @@
 import Ember from 'ember';
 import WebSocketConnection from 'decisions/utils/websocket-connection';
 
+const manifest = [
+  {
+    file: 'a.mp4',
+    optionAFile: 'b.mp4',
+    optionBFile: 'c.mp4',
+    optionAText: 'Meow!',
+    optionBText: 'Rawr!',
+    question: 'This is the question for a?'
+  },
+  {
+    file: 'b.mp4',
+  },
+  {
+    file: 'c.mp4',
+  }
+];
+
+function maxByKey(obj) {
+  let maxKey;
+  let maxVal;
+
+  for (let key in obj) {
+    if (Ember.isEmpty(maxVal) || obj[key] < maxVal) {
+      maxKey = key;
+      maxVal = obj[key];
+    } 
+  }
+
+  return maxKey;
+}
+
+function findByKey(key, keyValue, coll) {
+  for (let obj of coll) {
+    if (obj[key] === keyValue) {
+      return obj;
+    }
+  }
+}
+
 export default Ember.Component.extend({
   classNames: ['video-layout'],
 
@@ -8,7 +47,12 @@ export default Ember.Component.extend({
   optionAVotes: 0,
   optionBVotes: 0,
   optionCVotes: 0,
-  loading: true,
+  currentVideoFile: null,
+  displayQuestion: false,
+
+  currentVideoURL: Ember.computed('currentVideoData', function() {
+    return `videos/${this.get('currentVideoData.file')}`;
+  }),
 
   init() {
     this._super(...arguments);
@@ -17,15 +61,56 @@ export default Ember.Component.extend({
       url: `ws://${location.host}/ws`,
 
       onMessage: data => {
-        this.voteReceived(data.value);
+        this.voteReceived(JSON.parse(data).value);
       }
     });
 
-    wsConnection
-      .getWebSocket()
-      .then(() => this.set('loading', false));
+    wsConnection.getWebSocket();
 
     this.set('wsConnection', wsConnection);
+    this.set('currentVideoData', manifest[0]);
+  },
+
+  didInsertElement() {
+    this.initCurrentVideo();
+  },
+
+  initCurrentVideo() {
+    let el = this.$('video')[0];
+
+    el.addEventListener('ended', () => this.videoEnded(), true); 
+  },
+
+  videoEnded() {
+    this.set('displayQuestion', true);
+
+    setTimeout(() => {
+      this.playNextVideo();
+    }, 1000 * 15)
+  },
+
+  playNextVideo() {
+    let votes = this.getProperties('optionAVotes', 'optionBVotes', 'optionCVotes');
+    let maxVotesKey = maxByKey(votes);
+
+    let nextVideoFileKey = {
+      'optionAVotes': 'optionAFile',
+      'optionBVotes': 'optionBFile',
+      'optionCVotes': 'optionCFile'
+    }[maxVotesKey];
+
+    let nextVideoFile = this.get(`currentVideoData.${nextVideoFileKey}`);
+    let nextVideoData = findByKey('file', nextVideoFile, manifest);
+
+    this.setProperties({
+      displayQuestion: false,
+      currentVideoData: nextVideoData,
+      optionAVotes: 0,
+      optionBVotes: 0,
+      optionCVotes: 0
+    });
+
+    this.initCurrentVideo()
   },
 
   voteReceived(value) {
@@ -39,4 +124,4 @@ export default Ember.Component.extend({
       throw new Error(`Unknown value '${value}' received.`);
     }
   }
-})
+});
